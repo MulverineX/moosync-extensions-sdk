6117d7e0b81522287520181fea2a33f8e1068805
Sahil Gupte <sahilsachingupte@gmail.com>
2025-01-16 19:41:02 -0500

    Add license

--- wasm-extension-rs/src/lib.rs
+++ wasm-extension-rs/src/lib.rs
@@ -1,3 +1,35 @@
+// Moosync
+// Copyright (C) 2024, 2025  Moosync <support@moosync.app>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Moosync
+// Copyright (C) 2025 Moosync
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 pub use extism_pdk::{config, error, http, info, log, warn, HttpRequest, HttpResponse};
 use extism_pdk::{plugin_fn, FnResult, Json};
 use handler::{
@@ -9,7 +41,16 @@ use handler::{
     on_volume_changed, perform_account_login, scrobble, search,
 };
 use serde_json::Value;
-use types::{entities::{QueryableAlbum, QueryableArtist, QueryablePlaylist}, extensions::ExtensionProviderScope, songs::Song, ui::extensions::{AccountLoginArgs, CustomRequestReturnType, ExtensionAccountDetail, PlaybackDetailsReturnType, PlaylistAndSongsReturnType, PlaylistReturnType, PreferenceArgs, RecommendationsReturnType, SearchReturnType, SongReturnType, SongsWithPageTokenReturnType}};
+use types::{
+    entities::{QueryableAlbum, QueryableArtist, QueryablePlaylist},
+    extensions::ExtensionProviderScope,
+    songs::Song,
+    ui::extensions::{
+        AccountLoginArgs, CustomRequestReturnType, ExtensionAccountDetail,
+        PlaybackDetailsReturnType, PlaylistAndSongsReturnType, PlaylistReturnType, PreferenceArgs,
+        RecommendationsReturnType, SearchReturnType, SongReturnType, SongsWithPageTokenReturnType,
+    },
+};
 
 pub mod api;
 pub mod handler;
@@ -167,7 +208,7 @@ pub fn on_song_changed_wrapper() -> FnResult<Json<()>> {
 
 #[tracing::instrument(level = "trace", skip(time))]
 #[plugin_fn]
-pub fn on_seeked_wrapper(time: f64) -> FnResult<Json<()>> {
+pub fn on_seeked_wrapper(Json(time): Json<f64>) -> FnResult<Json<()>> {
     on_seeked(time)?;
     Ok(Json(()))
 }
